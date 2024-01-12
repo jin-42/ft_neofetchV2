f530b09f67045af131195a300b0ebf75378d3c92
jinmoonlab <faruksulvac@gmail.com>
2024-01-12 10:42:45 +0100

    add ft_neofetchV2

---
+++ ft_neofetch.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+os=$(uname -a | awk '{print $1}')
+hostname=$(uname -a | awk '{print $2}')
+kernel=$(uname -a | awk '{print $3}')
+arch=$(uname -a | awk '{print $13}')
+date=$(date)
+cpu=$(top -bn 1 | grep -e 'root' -e $USER | awk '{sum += $9} END {print sum}')
+mem_total=$(free -h | awk '/^Mem:/ {print $2}')
+mem_used=$(free -h | awk '/^Mem:/ {print $3}')
+image_path="anime-girl.txt"
+vendor=$(lscpu | grep -e "Vendor ID:" | awk '{print $3}')
+cpu_core=$(nproc)
+frequence=$(lscpu | grep -e "CPU max MHz:" | awk '{if (!printed) {printf "%0.f",  $4; printed=1}}')
+term=$(echo $TERM) 
+log=$(w | awk "NR==3" | awk '{print $4}')
+echo 
+echo -e "  ⠄⠄⠄⢰⣧⣼⣯⠄⣸⣠⣶⣶⣦⣾⠄⠄⠄⠄⡀⠄⢀⣿⣿⠄⠄⠄⢸⡇⠄⠄  \e[38;5;175m$USER@hostname\033[39m"
+echo -e "  ⠄⠄⠄⣾⣿⠿⠿⠶⠿⢿⣿⣿⣿⣿⣦⣤⣄⢀⡅⢠⣾⣛⡉⠄⠄⠄⠸⢀⣿⠄  \033[91mVendor: \033[39m$vendor"
+echo -e "  ⠄⠄⢀⡋⣡⣴⣶⣶⡀⠄⠄⠙⢿⣿⣿⣿⣿⣿⣴⣿⣿⣿⢃⣤⣄⣀⣥⣿⣿⠄  \033[91mCpu : \033[39m($cpu_core)core @$frequence MHz"
+echo -e "  ⠄⠄⢸⣇⠻⣿⣿⣿⣧⣀⢀⣠⡌⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⠿⣿⣿⣿⠄  \033[91mOS: \033[39m$os"
+echo -e "  ⠄⢀⢸⣿⣷⣤⣤⣤⣬⣙⣛⢿⣿⣿⣿⣿⣿⣿⡿⣿⣿⡍⠄⠄⢀⣤⣄⠉⠋⣰  \033[91mHostname: \033[39m$hostname"
+echo -e "  ⠄⣼⣖⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣿⣿⣿⣿⣿⢇⣿⣿⡷⠶⠶⢿⣿⣿⠇⢀⣤  \033[91mKernel: \033[39m$kernel"
+echo -e "  ⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣽⣿⣿⣿⡇⣿⣿⣿⣿⣿⣿⣷⣶⣥⣴⣿⡗  \033[91mArchitecture: \033[39m$arch"
+echo -e "  ⢀⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄  \033[91mTerminal: \033[39m$term"
+echo -e "  ⢸⣿⣦⣌⣛⣻⣿⣿⣧⠙⠛⠛⡭⠅⠒⠦⠭⣭⡻⣿⣿⣿⣿⣿⣿⣿⣿⡿⠃⠄  \033[91mDate: \033[39m$date"
+echo -e "  ⠘⣿⣿⣿⣿⣿⣿⣿⣿⡆⠄⠄⠄⠄⠄⠄⠄⠄⠹⠈⢋⣽⣿⣿⣿⣿⣵⣾⠃⠄  \033[91mUptime: \033[39m$log"
+echo -e "  ⠄⠘⣿⣿⣿⣿⣿⣿⣿⣿⠄⣴⣿⣶⣄⠄⣴⣶⠄⢀⣾⣿⣿⣿⣿⣿⣿⠃⠄⠄  \033[91mCPU Usage: \033[39m$cpu%"
+echo -e "  ⠄⠄⠈⠻⣿⣿⣿⣿⣿⣿⡄⢻⣿⣿⣿⠄⣿⣿⡀⣾⣿⣿⣿⣿⣛⠛⠁⠄⠄⠄  \033[91mMemory Usage: \033[39m$mem_used/$mem_total"
+echo -e "  ⠄⠄⠄⠄⠈⠛⢿⣿⣿⣿⠁⠞⢿⣿⣿⡄⢿⣿⡇⣸⣿⣿⠿⠛⠁⠄⠄⠄⠄⠄"
+echo -e "  ⠄⠄⠄⠄⠄⠄⠄⠉⠻⣿⣿⣾⣦⡙⠻⣷⣾⣿⠃⠿⠋⠁⠄⠄⠄⠄⠄⢀⣠⣴"
+echo -e "  ⣿⣿⣿⣶⣶⣮⣥⣒⠲⢮⣝⡿⣿⣿⡆⣿⡿⠃⠄⠄⠄⠄⠄⠄⠄⣠⣴⣿⣿⣿"
+echo
